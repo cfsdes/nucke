3b001a68defba88c9d9d426cbb044f5758b1ffdf
Henrique Scocco <henrique.scocco@gmail.com>
2023-04-03 17:21:06 -0300

    add jc-api flag

--- main.go
+++ main.go
@@ -8,19 +8,25 @@ import (
 	"net/http"
 	"net/http/httputil"
     "flag"
+    "encoding/json"
+    "bytes"
 )
 
-func parseFlags() (port int, timeout int) {
+
+func parseFlags() (port int, jcAPI string) {
 	flag.IntVar(&port, "port", 8080, "port number to use")
-	flag.IntVar(&timeout, "timeout", 60, "timeout in seconds")
+    flag.StringVar(&jcAPI, "jc-api", "http://127.0.0.1:5000", "jcAPI value to send to handler")
 	flag.Parse()
 	return
 }
 
 func main() {
-    port, _ := parseFlags()
+    port, jaelesApi := parseFlags()
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r, jaelesApi)
+	})
+
     server := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
 		Handler: http.DefaultServeMux,
@@ -34,7 +40,7 @@ func main() {
 }
 
 // Proxy Handler
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request, jaelesApi string) {
 	// Convert the raw request to base64
 	requestBytes, err := httputil.DumpRequest(r, true)
 	if err != nil {
@@ -45,6 +51,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	// Print the base64-encoded request
 	fmt.Println(requestBase64)
+    
+    //Send request to jaeles API server
+    sendToJaeles(requestBase64, jaelesApi)
 
     fowardRequest(w, r)
 	
@@ -73,4 +82,43 @@ func fowardRequest(w http.ResponseWriter, r *http.Request) {
         return
     }
     w.Write(body)
+}
+
+
+// Send request to jaeles API using the base64 target raw request
+func sendToJaeles(base64str string, jaelesApi string) error {
+    // Create request body as JSON
+    reqBody := map[string]string{
+        "req": base64str,
+    }
+    reqBytes, err := json.Marshal(reqBody)
+    if err != nil {
+        return err
+    }
+
+    // Create request
+    req, err := http.NewRequest("POST", jaelesApi + "/api/parse", bytes.NewReader(reqBytes))
+    if err != nil {
+        return err
+    }
+
+    // Add headers
+    req.Header.Add("User-Agent", "Jaeles Scanner")
+    req.Header.Add("Content-Type", "application/json")
+    req.Header.Add("Content-Length", string(len(reqBytes)))
+    req.Close = true
+
+    // Send request
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return fmt.Errorf("unexpected status code: %v", resp.StatusCode)
+    }
+
+    return nil
 }
