1bb0f1ca1d878376aeaea5f01a0d8d78aba6541f
Henrique Scocco <henrique.scocco@gmail.com>
2023-04-07 11:43:59 -0300

    ajustando comentario flag proxy

--- internal/utils/flags.go
+++ internal/utils/flags.go
@@ -13,13 +13,12 @@ func ParseFlags() (port int, jcAPI string, jc bool, scope string, proxy string, 
     flag.StringVar(&jcAPI, "jc-api", "http://127.0.0.1:5000", "jaeles API server")
     flag.BoolVar(&jc, "jc", false, "enable jaeles proxy")
 	flag.StringVar(&scope, "scope", "", "regex for scope")
-    flag.StringVar(&proxy, "proxy", "", "http proxy to use")
+    flag.StringVar(&proxy, "proxy", "", "http proxy to use during scans")
     flag.StringVar(&config, "config", "", "yaml config file with plugins to scan")
     flag.StringVar(&output, "out", "", "output directory to save scan results")
 
     // Add the welcome message to the --help output
 	flag.Usage = func() {
-		Banner()
         color.Cyan("Usage: \n")
         fmt.Fprintf(flag.CommandLine.Output(), "  %s [flags]\n\n", os.Args[0])
         color.Cyan("Flags: \n")
