87592cdd02829f4db2de2bf3e517ae9e2c73eb6e
Henrique Scocco <henrique.scocco@gmail.com>
2023-04-13 00:01:30 -0300

    update sample

--- examples/sample/sample.go
+++ examples/sample/sample.go
@@ -6,18 +6,22 @@ import (
 )
 
 
-func Run(r *http.Request, client *http.Client) (string, string, string, bool, error) {
-	// Code here
-
+func Run(r *http.Request, w http.ResponseWriter, client *http.Client, pluginDir string) (string, string, string, bool, error) {
+	// Scan
+    vulnFound := scan(r, w, client)
+    
+    // Report details
     var severity string = "High"
     var description string = "vulnerability description"
     var url string = "http://example.com"
-    var vulnFound bool = true
     
-    reportContent := template.ReadFileToString("report-template.txt")
+    reportContent := template.ReadFileToString("report-template.txt", pluginDir)
     summary := template.ParseTemplate(reportContent, map[string]interface{}{
         "description": description,
     })
 
     return	severity, url, summary, vulnFound, nil
 }
+
+
+func scan(r *http.Request, w http.ResponseWriter, client *http.Client) bool {}
