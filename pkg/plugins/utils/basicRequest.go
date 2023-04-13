e16aa503d3436309f8eb914215ec9b8af3d35b51
Henrique Scocco <henrique.scocco@gmail.com>
2023-04-13 09:24:30 -0300

    Adicionando mais retornos na basicRequest

--- pkg/plugins/utils/basicRequest.go
+++ pkg/plugins/utils/basicRequest.go
@@ -3,31 +3,42 @@ package utils
 import (
 	"net/http"
     "io/ioutil"
+    "time"
 )
 
 /**
 * Just reproduce the request provided
 */
 
-func BasicRequest(r *http.Request, w http.ResponseWriter, client *http.Client) (string, error) {
+func BasicRequest(r *http.Request, w http.ResponseWriter, client *http.Client) (int, string, int, map[string][]string, error) {
     req := CloneRequest(r, w)
 
     if client == nil {
         client = &http.Client{}
     }
-    
+
     // Send the request
+    start := time.Now()
     resp, err := client.Do(req)
     if err != nil {
-        return "", err
+        return 0, "", 0, nil, err
     }
     defer resp.Body.Close()
 
     // Read the response body
     responseBody, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        return "", err
+        return 0, "", 0, nil, err
     }
 
-    return string(responseBody), nil
+    // Get response time
+    elapsed := int(time.Since(start).Seconds())
+
+    // Get response headers
+    responseHeaders := make(map[string][]string)
+    for k, v := range resp.Header {
+        responseHeaders[k] = v
+    }
+
+    return elapsed, string(responseBody), resp.StatusCode, responseHeaders, nil
 }
