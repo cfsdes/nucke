d8c51c97371c9ca72316e7a985788a2507bb7407
Henrique Scocco <henrique.scocco@gmail.com>
2023-04-14 23:43:55 -0300

    adicionando checagem de binários

---
+++ internal/utils/checkBinaries.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+    "log"
+    "os/exec"
+)
+
+func CheckBinaries(binaries []string) {
+    for _, binary := range binaries {
+        cmd := exec.Command(binary, "--help")
+        if err := cmd.Run(); err != nil {
+            if exitError, ok := err.(*exec.ExitError); ok {
+                if exitError.ExitCode() == 127 {
+                    log.Fatal("command '" + binary + "' not found")
+                }
+            } else {
+                log.Fatal("command '" + binary + "' not found")
+            }
+        }
+    }
+}
