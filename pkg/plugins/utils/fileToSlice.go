92c5ae6f017a0de66067a435780e3e0a4fd62151
Henrique Scocco <henrique.scocco@gmail.com>
2023-04-26 10:13:37 -0300

    criando module fileToSlice

---
+++ pkg/plugins/utils/fileToSlice.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+    "os"
+    "bufio"
+)
+
+// Read file and return a slice
+func FileToSlice(pluginDir string, rulesFile string) ([]string) {
+	
+    // if rulesFile is empty, use the default name
+	if rulesFile == "nil" {
+		rulesFile = "regex_match.txt"
+	}
+
+    // Open the file for reading
+    file, err := os.Open(pluginDir + "/" + rulesFile)
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+
+    // Create an empty []string array
+    var lines []string
+
+    // Create a scanner to read from the file
+    scanner := bufio.NewScanner(file)
+
+    // Iterate over the lines in the file
+    for scanner.Scan() {
+        // Add the line to the []string array
+        lines = append(lines, scanner.Text())
+    }
+
+    return lines
+}
