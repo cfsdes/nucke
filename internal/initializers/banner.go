7c773c6f9f09919983cd21d62f4616f236f11fe3
Henrique Scocco <henrique.scocco@gmail.com>
2023-04-28 16:35:52 -0300

    Ajustando coloracao banner

--- internal/initializers/banner.go
+++ internal/initializers/banner.go
@@ -8,17 +8,21 @@ import (
 
 func Banner() {
     // Print a colorful welcome message
-    white := color.New(color.FgWhite, color.Bold)
+    cyan := color.New(color.FgCyan, color.Bold).SprintFunc()
 
-    white.Printf(`
+    nucke := fmt.Sprint(`
+                 _   __              __       
+                / | / /__  __ _____ / /__ ___ 
+               /  |/ // / / // ___// //_// _ \
+              / /|  // /_/ // /__ / ,<  /  __/
+             /_/ |_/ \__,_/ \___//_/|_| \___/ 
+`)
+
+    fmt.Printf(`
            *                       *
                         .
 
-                    _   __              __       
-                   / | / /__  __ _____ / /__ ___ 
-                  /  |/ // / / // ___// //_// _ \
-                 / /|  // /_/ // /__ / ,<  /  __/
-                /_/ |_/ \__,_/ \___//_/|_| \___/ 
+%s 
                                                       
    *           
                
@@ -37,7 +41,7 @@ _/\_/\_/\__  _/_/\_/\_/\_/\_/\_/\_/\_/\_/\_
 |  |  |  |(_(  |  |  |  |  |  |  |  |  |  |
 |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
 |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
-`)
+`, cyan(nucke))
 
     fmt.Println()
 }
