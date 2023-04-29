2c7d8d80e84930249348d447cb386f14b0f00186
Henrique Scocco <henrique.scocco@gmail.com>
2023-04-28 22:50:07 -0300

    Ajustando cores do output

--- internal/initializers/output.go
+++ internal/initializers/output.go
@@ -21,20 +21,19 @@ func VulnerabilityOutput(scanName string, severity string, url string, summary s
 	green := color.New(color.FgGreen, color.Bold).SprintFunc()
 	blue := color.New(color.FgBlue, color.Bold).SprintFunc()
 	magenta := color.New(color.FgMagenta, color.Bold).SprintFunc()
-	//cyan := color.New(color.FgCyan, color.Bold).SprintFunc()
-	//white := color.New(color.FgWhite, color.Bold).SprintFunc()
+	cyan := color.New(color.FgCyan).SprintFunc()
 
 	switch severity {
 	case "Critical":
-		fmt.Printf("[%s] [%s] %s \n", magenta(scanName), magenta(severity), url)
+		fmt.Printf("[%s] [%s] %s \n", cyan(scanName), magenta(severity), url)
 	case "High":
-		fmt.Printf("[%s] [%s] %s \n", red(scanName), red(severity), url)
+		fmt.Printf("[%s] [%s] %s \n", cyan(scanName), red(severity), url)
 	case "Medium":
-		fmt.Printf("[%s] [%s] %s \n", yellow(scanName), yellow(severity), url)
+		fmt.Printf("[%s] [%s] %s \n", cyan(scanName), yellow(severity), url)
 	case "Low":
-		fmt.Printf("[%s] [%s] %s \n", green(scanName), green(severity), url)
+		fmt.Printf("[%s] [%s] %s \n", cyan(scanName), green(severity), url)
 	case "Info":
-		fmt.Printf("[%s] [%s] %s \n", blue(scanName), blue(severity), url)
+		fmt.Printf("[%s] [%s] %s \n", cyan(scanName), blue(severity), url)
 	}
 
 	// Create output
