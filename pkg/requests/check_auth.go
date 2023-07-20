e0f6c9957f6ea5a3307fa1a6aaa428d049916cb3
Henrique Scocco <henrique.scocco@gmail.com>
2023-07-19 22:50:53 -0300

    ajustando checkauth

--- pkg/requests/check_auth.go
+++ pkg/requests/check_auth.go
@@ -2,6 +2,7 @@ package requests
 
 import (
 	"net/http"
+	"github.com/cfsdes/nucke/pkg/plugins/utils"
 )
 
 // Return true if request require authentication
@@ -9,16 +10,17 @@ func CheckAuth(r *http.Request, client *http.Client) bool {
 	req := CloneReq(r)
 
 	// Send Original Request
-    _, _, originalStatusCode, _, _ := BasicRequest(req, client)
+    _, originalResBody, _, _, _ := BasicRequest(req, client)
 
     // Remove auth headers
     req.Header.Del("Cookie")
     req.Header.Del("Authorization")
     
 	// Send unauth request
-	_, _, unauthStatusCode, _, _ := BasicRequest(req, client)
+	_, unauthResBody, _, _, _ := BasicRequest(req, client)
 
-	if (originalStatusCode != unauthStatusCode) {
+	// Compare if original response is less than 95% equal from the unauth Response
+	if (utils.TextSimilarity(originalResBody, unauthResBody) <= 0.95) {
 		return true
 	} else {
 		return false
