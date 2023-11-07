f21f1557ddc277acd68761db79e6e1175bf76edb
Henrique Scocco <henrique.scocco@gmail.com>
2023-11-07 15:04:27 -0300

    adjust check_auth

--- pkg/requests/check_auth.go
+++ pkg/requests/check_auth.go
@@ -2,9 +2,7 @@ package requests
 
 import (
 	"net/http"
-	"fmt"
 
-	"github.com/cfsdes/nucke/pkg/globals"
 	"github.com/cfsdes/nucke/pkg/plugins/utils"
 )
 
@@ -20,15 +18,8 @@ func CheckAuth(r *http.Request, client *http.Client) bool {
     req.Header.Del("Authorization")
     
 	// Send unauth request
-	resp, err := client.Do(req)
-    if err != nil {
-        if globals.Debug {
-            fmt.Println("CheckAuth Request Error:",err)
-        }
-    }
+	_, unauthResBody, statusCode, _, _ := BasicRequest(req, client)
 
-	// Get response body
-    statusCode, unauthResBody, _, _ := ParseResponse(resp)
 
 	// Compare if original response is less than 90% equal from the unauth Response
 	if (utils.TextSimilarity(originalResBody, unauthResBody) < 0.9 && statusCode != 429 && originalStatusCode < 300) {
